a8a0e738da0cb0ab5b66ddf1dcff5d2f27986781
Adam Siyala <69278320+AdamSiyala@users.noreply.github.com>
2021-05-11 16:49:57 -0700

    ordering of functions changed

--- server.js
+++ server.js
@@ -17,13 +17,15 @@ app.use(logger('dev'));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
 
+const port = process.env.PORT || 3000;
+
 const config = {
   authRequired: false,
   auth0Logout: true,
   baseURL: `http://localhost:${port}`
 };
 
-const port = process.env.PORT || 3000;
+
 if (process.env.NODE_ENV === 'production') {
   config.baseURL = process.env.BASE_URL
 }
