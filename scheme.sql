2ef4b7aadf3266ae9e57bb020fd6570f5e82d132
Kya Bam (Chanel) <71570873+Chanelbam@users.noreply.github.com>
2021-05-13 15:56:36 -0700

    Delete scheme.sql

--- scheme.sql
+++
@@ -1,8 +0,0 @@
-create database heroku_03c6ca63989ce29;
-use heroku_03c6ca63989ce29;
-
-CREATE TABLE note (
-  note_id INTEGER PRIMARY KEY AUTO_INCREMENT,
-  note_title VARCHAR(45),
-  note_body VARCHAR(500),
-);
